a46ce62f7327203136f1ae6bb1762cdfecf98b55
Achim <achim@zibra.de>
2021-03-12 15:37:24 +0100

    Add testcode for Record with avro serialized key (with decorator)

--- src/test/java/org/akhq/models/RecordTest.java
+++ src/test/java/org/akhq/models/RecordTest.java
@@ -1,10 +1,21 @@
 package org.akhq.models;
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import org.akhq.Breed;
+import org.akhq.Cat;
+import org.akhq.models.decorators.AvroKeySchemaRecord;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class RecordTest {
 
@@ -27,11 +38,27 @@ public class RecordTest {
     @Test
     public void testKeyIsAvroSerializedWithDecorator() {
 
-        // GIVEN a record with an avro serialized key
-        // AND a schema id is given
+        // Testdata
+        String avroCatExampleJson = "{\"id\":10,\"name\":\"Tom\",\"breed\":\"SPHYNX\"}";
+        GenericRecord avroCatExample = aCatExample(10, "Tom", Breed.SPHYNX);
+
+        // Mocks
+        Deserializer<Object> aMockedAvroDeserializer = Mockito.mock(KafkaAvroDeserializer.class);
+        Mockito.when(aMockedAvroDeserializer.deserialize(Mockito.any(), Mockito.any())).thenReturn(avroCatExample);
+
+        // GIVEN a decorated record with an avro serialized key and a schema id
+        byte[] keyBytes = avroCatExampleJson.getBytes(StandardCharsets.UTF_8);
+        byte[] valueBytes = "".getBytes(StandardCharsets.UTF_8); // value property does not matter
+
+        ConsumerRecord<byte[], byte[]> kafkaRecord = new ConsumerRecord<>("topic", 0, 0, keyBytes, valueBytes);
+        Record record = new Record(kafkaRecord, 1, null);
+        record = new AvroKeySchemaRecord(record, aMockedAvroDeserializer);
+
         // WHEN getKey() method is called
+        String keyString = record.getKey();
+
         // EXPECT a json result in String type
-        Assertions.fail();
+        assertThat(keyString, is(avroCatExampleJson));
     }
 
     @Test
@@ -77,5 +104,16 @@ public class RecordTest {
         // WHEN getKey() method is called
         // EXPECT a string representation of the key bytes as result
         Assertions.fail();
+    }
+
+    /**
+     * Method returns an avro example data object with a cat schema
+     */
+    private GenericRecord aCatExample(int id, String name, Breed breed) {
+        return new GenericRecordBuilder(Cat.SCHEMA$)
+                .set("id", id)
+                .set("name", name)
+                .set("breed", breed)
+                .build();
     }
 }
