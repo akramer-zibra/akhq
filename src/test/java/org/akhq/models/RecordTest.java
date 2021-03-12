a2eb5f7a703476835256d76083a2106249ff34e3
Achim <achim@zibra.de>
2021-03-12 14:00:23 +0100

    Add some stubs and specifications for record getKey() testcode

---
+++ src/test/java/org/akhq/models/RecordTest.java
@@ -0,0 +1,71 @@
+package org.akhq.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RecordTest {
+
+    @Test
+    public void testKeyByteArrayIsNull() {
+
+        // GIVEN a record with null in key
+        // WHEN getKey() method is called
+        // EXPECT NULL result
+        Assertions.fail();
+    }
+
+    @Test
+    public void testKeyIsAvroSerialized() {
+
+        // GIVEN a record with an avro serialized key
+        // AND a schema id is given
+        // WHEN getKey() method is called
+        // EXPECT a json result in String type
+        Assertions.fail();
+    }
+
+    @Test
+    public void testKeyIsAvroSerializedFallback() {
+
+        // GIVEN a record with an avro serialized key
+        // AND a schema id is given
+        // WHEN getKey() method is called
+        // AND kafka avro deserializer throws an exception
+        // EXPECT a string representation of the key bytes as result
+        Assertions.fail();
+    }
+
+    @Test
+    public void testKeyIsProtobufSerialized() {
+
+        // GIVEN a record with a protobuf serialized key
+        // AND no schema id is given
+        // AND protobufToJsonDeserializer exists
+        // WHEN getKey() method is called
+        // EXPECT a protobuf deserialized value in String type as result
+        Assertions.fail();
+    }
+
+    @Test
+    public void testKeyIsProtobufSerializedFallback() {
+
+        // GIVEN a record with a protobuf serialized key
+        // AND no schema id is given
+        // AND protobufToJsonDeserializer exists
+        // WHEN getKey() method is called
+        // AND protobuf deserializer throws an exception
+        // EXPECT a string representation of the key bytes as result
+        Assertions.fail();
+    }
+
+    @Test
+    public void testKeyDefaultFallback() {
+
+        // GIVEN a record with a protobuf serialized key
+        // AND no schema id exists
+        // AND no protobufToJsonDeserializer exists
+        // WHEN getKey() method is called
+        // EXPECT a string representation of the key bytes as result
+        Assertions.fail();
+    }
+}
